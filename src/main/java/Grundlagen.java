bc2fc17ec98a8a44729f9f4f94a498ea8b9bae2e
Marco Harfmann <marcohfm@yahoo.de>
2023-09-16 12:50:17 +0200

    Initial commit

---
+++ src/main/java/Grundlagen.java
@@ -0,0 +1,14 @@
+public class Grundlagen {
+
+public static void main(String[] args){
+    System.out.println("Start Test");
+
+    int alter=3;
+
+    String name="Tom";
+
+    System.out.println(name+" ist "+alter +" Jahre alt.");
+
+}
+
+}
